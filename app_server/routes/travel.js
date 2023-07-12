0b7a37ca86dec44c6625c81a064096574a0fdbf8
Jonathan Vest <jonathan.vest@snhu.edu>
2023-07-12 11:00:14 -0400

    Static HTML to Templates With JSON

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/travel');
+const express = require('express');
+const router = express.Router();
+const controller= require('../controllers/travel');
 
 /* GET home page. */
 router.get('/', controller.travel);
